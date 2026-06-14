fc5a567c433b18408b01a021dbd65b56fb9f4174
carloscorcolesbenavides <carlos.corcoles.benavides@gmail.com>
2026-06-14 14:24:41 +0200

    Update 'text2cypher_rag.py'

--- text2cypher_rag.py
+++ text2cypher_rag.py
@@ -1,3 +1,7 @@
+"""
+Created "from https://graphacademy.neo4j.com/courses/genai-fundamentals/4-integrating-neo4j/5-text-to-cypher/"
+"""
+#-----------------------------------------------------------------------------------------
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,7 +10,7 @@ from neo4j import GraphDatabase
 from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.generation import GraphRAG
 from neo4j_graphrag.retrievers import Text2CypherRetriever
-
+#-----------------------------------------------------------------------------------------
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
     os.getenv("NEO4J_URI"), 
